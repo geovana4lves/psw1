fece50989bb400914f66d887694911844c629c6b
Geovana Alves <geovanaalvesgbi4@gmail.com>
2024-11-13 16:43:33 -0300

    oi

---
+++ aula-13-11/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+<ul>
+        <li><a href="https://www.ifbaiano.edu.br/unidades/guanambi/2024/08/29/viii-seapo-comeca-nesta-sexta-feira-e-no-sabado-tem-feira-agroecologica-no-centro-de-guanambi/">SITE.IF</a></li>
+        <li> <a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.instagram.com/ifbaianoguanambi/&ved=2ahUKEwictL-F_NmJAxXzpZUCHV_QN_gQFnoECGAQAQ&usg=AOvVaw2gvoNLAlDUCZjAiOxtjSZC">INSTAGRAM.IF</a></li>
+        <li> <a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.facebook.com/ifacontece/%3Flocale%3Dpt_BR&ved=2ahUKEwjtyciw_9mJAxWEALkGHfq4HygQFnoECBsQAQ&usg=AOvVaw0-9WScWNASzCn7KZTQ3XHZ">FACEBOOK.IF</a></li>
+    </ul> 
+</body>
+</html>
